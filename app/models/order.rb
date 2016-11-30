280eff53e375f3c2620f7977bc64fdede6736647
JoseDLopez <josed-lopez@hotmail.com>
2016-11-30 01:53:50 -0300

    Relationship between orders and products is completed

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,8 @@
 class Order < ApplicationRecord
-  belongs_to :client
-  belongs_to :zone
-  belongs_to :payment
+	belongs_to :client
+	belongs_to :zone
+	belongs_to :payment
+
+	has_many :order_products
+	has_many :products, through: :order_products
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130044512) do
+ActiveRecord::Schema.define(version: 20161130044937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20161130044512) do
     t.string   "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "order_products", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id", using: :btree
+    t.index ["product_id"], name: "index_order_products_on_product_id", using: :btree
   end
 
   create_table "orders", force: :cascade do |t|
@@ -87,6 +96,8 @@ ActiveRecord::Schema.define(version: 20161130044512) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "orders", "clients"
   add_foreign_key "orders", "payments"
   add_foreign_key "orders", "zones"
