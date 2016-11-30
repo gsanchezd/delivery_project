6fcd83bb8da79fcff77a712e9f60c0f5828df2ab
JoseDLopez <josed-lopez@hotmail.com>
2016-11-29 22:03:44 -0300

    We make the relationship for products and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130004716) do
+ActiveRecord::Schema.define(version: 20161130005016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 20161130004716) do
     t.string   "status"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id", using: :btree
+    t.index ["product_id"], name: "index_product_categories_on_product_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -52,4 +61,6 @@ ActiveRecord::Schema.define(version: 20161130004716) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
