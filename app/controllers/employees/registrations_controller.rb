eba596f53ec748e14a8872a3b17b93a0d3f041ae
JoseDLopez <josed-lopez@hotmail.com>
2016-12-03 02:09:42 -0300

    We comment some mistake on registration controller

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -38,12 +38,12 @@ class Employees::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
-  If you have extra params to permit, append them to the sanitizer.
+  #If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  If you have extra params to permit, append them to the sanitizer.
+  #If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
