8af9c4f903d66161d8b9758bf490244be80ce6d0
JoseDLopez <josed-lopez@hotmail.com>
2016-12-03 14:20:24 -0300

    We change the redirection path for users not logged or authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
-	before_action :authenticate_employee!, except: [:home, :status]
+	# before_action :authenticate_employee!, except: [:home, :status]
+	before_filter :auth_employee, except: [:home, :status]
 	protect_from_forgery with: :exception
+
+	def auth_employee
+		unless employee_signed_in?
+			redirect_to root_path
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   get 'pages/livemap'
 
-  root 'pages#home'
+  root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
